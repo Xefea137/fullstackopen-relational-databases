201b4d36d7b3880132c196c1a9fd1303f2135009
Moe Khan <mobashir16@gmail.com>
2024-11-14 09:10:18 +0530

    13.4

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -8,9 +11,74 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false
     }
   },
+});
+
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
 })
 
-const main = async () => {
+Blog.sync()
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  // console.log(blogs.map(b=>b.toJSON()))
+  console.log(JSON.stringify(blogs, null, 2))
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    console.log(req.body)
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const id = req.params.id
+  if (id) {
+    await Blog.destroy({
+      where: { id }
+    })
+    res.status(204).end()
+  } else {
+    res.status(404).end()
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)  
+})
+
+/*const main = async () => {
   try {
     await sequelize.authenticate()
     console.log('Connection has been established successfully.')
@@ -23,4 +91,4 @@ const main = async () => {
   }
 }
 
-main()
+main()*/
