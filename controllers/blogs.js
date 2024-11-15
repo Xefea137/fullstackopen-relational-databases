b5e476586ae3eeeb9a3cd6f42b0bcae79d698dc7
Moe Khan <mobashir16@gmail.com>
2024-11-15 08:45:53 +0530

    13.5 - 13.7

---
+++ controllers/blogs.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', async (req, res) => {
+  const blog = await Blog.create(req.body)
+  res.json(blog)
+})
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(400).end()
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
+  }
+  res.status(204).end()
+})
+
+module.exports = router
